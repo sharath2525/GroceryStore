abe92ee89392ffa2b01d6a87973022f5fdc88fe8
sharath2525 <donthulasharath25@gmail.com>
2025-01-26 17:56:20 +0530

    revoking id and names change s

--- GroceryStore/Models/Product.cs
+++ GroceryStore/Models/Product.cs
@@ -1,7 +1,27 @@
-﻿namespace GroceryStore.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace GroceryStore.Models
 {
     public class Product
     {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [ValidateNever]
+        public string ImageUrl { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public string Quantity { get; set; }
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        [ValidateNever]
+        public Category Category { get; set; }
 
     }
 }
